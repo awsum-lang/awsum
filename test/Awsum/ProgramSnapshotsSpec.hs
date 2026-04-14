bc50fe73918d304491ef441b6a7898fecc146838
Vladimir Logachev <vladimir@logachev.dev>
2026-04-14 17:55:17 +0400

    Extract formatting tests

--- test/Awsum/ProgramSnapshotsSpec.hs
+++ test/Awsum/ProgramSnapshotsSpec.hs
@@ -11,7 +11,6 @@ import Awsum.Codegen.WASM (codegenWASM)
 import Awsum.Codegen.WASM.Assemble (assembleWASM)
 import Awsum.Core
 import Awsum.ElaborateLower (elaborateLowerProgram)
-import Awsum.Format (formatSource)
 import Awsum.Parser (parseProgram)
 import Awsum.Syntax
 import Common.File
@@ -40,7 +39,6 @@ spec = describe "Program snapshots" $ do
   testProgram "adt-single-parameter-single-constructor-with-data-nested-case.aww" ["adt-single-parameter-single-constructor-with-data-nested-case.input1.txt"]
   testProgram "adt-empty-type.aww" ["adt-empty-type.input1.txt"]
   testProgram "adt-empty-type-nested.aww" ["adt-empty-type-nested.input1.txt"]
-  testProgram "improperly-formatted-source.aww" ["improperly-formatted-source.input1.txt"]
   testProgram "recursive-function-call.aww" ["recursive-function-call.input1.txt"]
   testProgram "adt-result.aww" ["adt-result.input1.txt"]
   testProgram "adt-result-nested-string.aww" ["adt-result-nested-string.input1.txt"]
@@ -57,7 +55,6 @@ sourcesDir = "test/sources/successful/"
 data CompileResult = CompileResult
   { ast :: Program,
     core :: CoreProgram,
-    formattedSource :: Text,
     llvmCompiledCode :: Text,
     jvmCompiledCode :: Text,
     jvmClassBytes :: ByteString,
@@ -78,14 +75,10 @@ compileAll sourceFile = do
   core <- case elaborateLowerProgram ast of
     Left err -> error $ "elaborate failed" <> show err
     Right x -> pure x
-  formattedSource <- case formatSource src of
-    Left err -> error $ "format failed" <> show err
-    Right x -> pure x
   pure
     CompileResult
       { ast = ast,
         core = core,
-        formattedSource = formattedSource,
         llvmCompiledCode = codegenLLVM core,
         jvmCompiledCode = codegenJVM core,
         jvmClassBytes = assembleJVM core,
@@ -112,8 +105,6 @@ testProgram sourceFile inputFiles = do
       res.ast `shouldMatchShowSnapshot` (snap <> "/ast.txt")
     it "Core should match snapshot" $ \res -> do
       res.core `shouldMatchShowSnapshot` (snap <> "/core.txt")
-    it "Formatted source should match snapshot" $ \res -> do
-      res.formattedSource `shouldMatchTextSnapshot` (snap <> "/formatted." <> sourceFile)
     it "LLVM code should match snapshot" $ \res -> do
       res.llvmCompiledCode `shouldMatchTextSnapshot` (snap <> "/compiled.ll")
     it "JVM code should match snapshot" $ \res -> do
